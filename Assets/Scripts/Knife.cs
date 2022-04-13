3a0c6edb47d8b8a0a0fc06fa0e0297ab5a41e372
hodat140600 <hodat140600@gmail.com>
2022-04-13 14:37:59 +0700

    First commit

---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,81 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _FlippyKnife
+{
+    public class Knife : MonoBehaviour {
+
+        public static Knife Instance;
+
+        public Rigidbody rb;
+        public float force = 5f;
+        public float torque = 20f;
+        private float timeStartFlying;
+
+        private Vector2 startSwipe;
+        private Vector2 endSwipe;
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+            // lấy tọa độ từ lần nhấn chuột đầu kéo tới điểm thả chuột cuối
+		    if (Input.GetMouseButtonDown(0))
+            {
+                startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);// từ tọa độ màn hình pixel đưa về tọa độ 0 1
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                Swipe();
+            }
+	    }
+        void Swipe()// phương thức vuốt để tung dao
+        {
+            rb.isKinematic = false;
+            timeStartFlying = Time.time;
+            Vector2 swipe = endSwipe - startSwipe;
+            //Debug.Log(swipe);
+            rb.AddForce(swipe * force, ForceMode.Impulse);// đưa lực vào rb 
+            rb.AddTorque(0f, 0f, -torque, ForceMode.Impulse);// đưa lực để xoay trục z
+
+        }
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.tag == "Block")
+            {
+                rb.isKinematic = true;
+                if (rb.isKinematic)//tính điểm khi bắt va chạm
+                {
+                    ScoreManager.Instance.AddScore(1);
+                    UIManager.Instance.txtScore.GetComponent<Text>().text = ScoreManager.Instance.GetScore().ToString();
+                    UIManager.Instance.txtScoreBest.GetComponent<Text>().text = ScoreManager.Instance.GetScoreBest().ToString();
+                }
+            }
+            else
+            {
+                Restart();
+            }
+        }
+        void OnCollisionEnter()
+        {
+            float timeInAir = Time.time - timeStartFlying;
+            if (!rb.isKinematic && timeInAir >= 0.05f)
+            {
+                Debug.Log("FAIL");
+                Restart();
+            }
+            
+        }
+    
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _FlippyKnife
+{
+    //cái này là cách tính điểm tui xem từ pj dodge the block
+    public class ScoreManager : MonoBehaviour {
+
+        public static ScoreManager Instance;
+
+        private int score;
+        private int scoreBest;
+        private void Awake()
+        {
+            if (!Instance)
+                Instance = this;
+        }
+
+        private void Start()
+        {
+            scoreBest = PlayerPrefs.GetInt("SCOREBEST");
+            UIManager.Instance.txtScoreBest.GetComponent<Text>().text = scoreBest.ToString();
+        }
+        public int GetScore()
+        {
+            return score;
+        }
+        public int GetScoreBest()
+        {
+            return scoreBest;
+        }
+
+        public void AddScore(int n)
+        {
+            score += n;
+            if (scoreBest < score)
+            {
+                scoreBest = score;
+                UIManager.Instance.txtScoreBest.GetComponent<Text>().text = scoreBest.ToString();
+                // Save scoreBest 
+                PlayerPrefs.SetInt("SCOREBEST", scoreBest);
+            }
+            //Debug.Log(score);
+            UIManager.Instance.txtScore.GetComponent<Text>().text = score.ToString();
+        }
+    }
+}
